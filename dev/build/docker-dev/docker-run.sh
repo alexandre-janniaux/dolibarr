eaa6ad26839860491e92347680263386ec952d59
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2025-01-08 14:32:44 +0100

    docker-compose fixes

--- dev/build/docker-dev/docker-run.sh
+++ dev/build/docker-dev/docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script used by the Dockerfile.
 # See README.md to know how to create a Dolibarr env with docker
+set -ex
 
 usermod -u "${HOST_USER_ID}" www-data
 groupmod -g "${HOST_USER_ID}" www-data
